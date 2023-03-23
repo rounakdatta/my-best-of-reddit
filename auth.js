3f946b912f3569da83eb5912e49139f34cb4e35a
Rounak Datta <rounakdatta12@gmail.com>
2023-03-23 12:03:03 +0530

    Initial commit for scraping and auth

---
+++ auth.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+
+// this function generates the short-lived access token from the fixed credentials
+async function getRedditAccessToken() {
+    const credentials = await getRedditCredentials();
+
+    const url = 'https://www.reddit.com/api/v1/access_token';
+    const body = `grant_type=password&username=${credentials.username}&password=${credentials.password}`;
+    const basicAuth = `${credentials.clientId}:${credentials.clientSecret}`;
+    const authenticationHeader = `Basic ${Buffer.from(basicAuth).toString('base64')}`;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        body: body,
+        headers: {
+            'Authorization': authenticationHeader
+        }
+    })
+
+    // occasionally the endpoint throws 429 (too many requests)
+    if (!response.ok) {
+        throw new Error(`Access token request calling failed with status code ${response.status}`)
+    }
+
+    const responseBody = await response.json()
+    return responseBody.access_token
+}
+
+async function getRedditCredentials() {
+    return {
+        'clientId': process.env.CLIENT_ID,
+        'clientSecret': process.env.CLIENT_SECRET,
+        'username': process.env.USERNAME,
+        'password': process.env.PASSWORD
+    }
+}
---
+++ shareddit.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+// this function downloads the post as image through Shareddit
+async function downloadRedditPostAsImage(postId, accessToken) {
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    headless: false
+    });
+  const page = await browser.newPage();
+  
+  // Navigate to website and click button
+  await page.goto(`https://shareddit.taptappers.club/generate?sub=IAmA&postId=${postId}&accessToken=${accessToken}`);
+  console.log('opened page, waiting for page to load completely...')
+  
+  await page.waitForTimeout(10000);
+  await page.click('.chakra-button');
+
+  
+  console.log('download initiated, waiting for it to finish')
+  await page.waitForTimeout(10000);
+  
+  console.log('closing browser')
+  await browser.close();
+};
